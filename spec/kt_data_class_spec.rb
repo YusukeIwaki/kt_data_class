3d79e4a32d01442f0779a8f7dfc785a46e195ffb
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2018-12-27 18:32:34 +0000

    remove stub spec

--- spec/kt_data_class_spec.rb
+++ spec/kt_data_class_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe KtDataClass do
   it "has a version number" do
     expect(KtDataClass::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
